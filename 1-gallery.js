b8db02a7a39168ecca93451ab200eec2eae22ce4
Anna-Klimenko13 <anna.klimenko6909@gmail.com>
2026-06-09 09:54:51 +0000

    Deploying to gh-pages from @ Anna-Klimenko13/goit-js-hw-09@02b65f24df13837f3d93a12a8d7d96b7a00668ca 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as p}from"./assets/vendor-CgTBfC_f.js";const t=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/riga-4202820__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/riga-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Ship"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake"}],e=document.querySelector(".gallery"),n=t.map(({preview:a,original:i,description:o})=>`
+/* empty css                      */import{S as c}from"./assets/vendor-CgTBfC_f.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const s=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/riga-4202820__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/riga-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Ship"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake"}],l=document.querySelector(".gallery"),p=s.map(({preview:a,original:o,description:r})=>`
     <li class="gallery-item">
-      <a class="gallery-link" href="${i}">
+      <a class="gallery-link" href="${o}">
         <img
           class="gallery-image"
           src="${a}"
-          alt="${o}"
+          alt="${r}"
         />
       </a>
-    </li>`).join("");e.innerHTML=n;new p(".gallery a",{captionsData:"alt",captionDelay:250});
+    </li>`).join("");l.innerHTML=p;new c(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
